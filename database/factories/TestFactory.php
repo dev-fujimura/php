5b93d21b8597035514f7ee4240374025a2ade0e7
dev-fujimura <68439390+dev-fujimura@users.noreply.github.com>
2020-07-18 03:44:34 +0900

    Add files via upload

---
+++ database/factories/TestFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\test;
+use Faker\Generator as Faker;
+
+$factory->define(test::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
